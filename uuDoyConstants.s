1c31b7b8738c2822b6bec3275846e2612b4ca413
Ton Smeele <a.p.m.smeele@uu.nl>
2017-06-20 11:38:59 +0200

    initial

---
+++ uuDoyConstants.s
@@ -0,0 +1,9 @@
+# \file
+# \copyright Copyright (c) 2016, Utrecht university. All rights reserved
+# \license GPLv3, see LICENSE
+
+# \constant UUPRIMARYRESOURCES
+UUPRIMARYRESOURCES = list("irodsResc");
+
+# \constant UUREPLICATIONRESOURCE
+UUREPLICATIONRESOURCE = "irodsRescRepl";
